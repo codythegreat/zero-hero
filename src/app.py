634611c8f6962f49a1c8029eaa824daf1fdf67c7
codythegreat <maxiecody@gmail.com>
2020-04-08 18:34:49 -0500

    load users for selection on startup

--- src/app.py
+++ src/app.py
@@ -1,10 +1,20 @@
 import PySimpleGUI as sg
 
-# Layout allows selection of existing user or new user
-layout = [
-    #TODO: existing user
+# model imports
+import models.User as User
+import models.Transaction as Transaction
 
-    # or create a new user
+# Layout allows selection of existing user or new user
+layout = []
+
+# get list of users and append to start of layout for selection
+users = User.readUsers()
+for user in users:
+    layout.append([sg.Text(f"{user.get('fname', '')} {user.get('lname', '')} - {user.get('email', '')}")])
+    layout.append([sg.Button('Select', key=user.get('id'))])
+
+# add user creation form to layout
+layout += [
     [sg.Text('Create a new user:')],
     [sg.Text('First Name')],
     [sg.InputText(key='fname')],
@@ -24,10 +34,22 @@ while True:
         pass
     # handle new user creation
     elif (event == 'Create'):
+        # add a new user
+        user = User.User(values.get('fname'), values.get('lname'), values.get('email'))
+        user.generate_user_file()
+        user.generate_transactions_file()
         pass
     # on 'None' exit
     elif event is None:
         break
+
+    # else, get the selected user and continue
+    else:
+        for user in users:
+            if user.get('id') == event:
+                print(f"{user.get('fname')} {user.get('lname')}")
+        #TODO: go to transactions screen
+
 
     # log event and values to console for debugging
     print(event, ' -- ', values)
